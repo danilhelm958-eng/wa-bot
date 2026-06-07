ca1a69edbea1fb9c5df992b5a538f9f02d102990
danilhelm958-eng <danilhelm958@gmail.com>
2026-06-07 12:38:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.post("/send", (req, res) => {
   res.json({ status: "ok", number, message });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log("Bot jalan di port " + PORT);
 });
