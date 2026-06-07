a9b3c480e10289c39c5c706c3d0ff390416625ad
danilhelm958-eng <danilhelm958@gmail.com>
2026-06-07 12:52:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,78 +1,20 @@
 const express = require("express");
-const {
-    default: makeWASocket,
-    useMultiFileAuthState,
-    DisconnectReason
-} = require("@whiskeysockets/baileys");
-
 const app = express();
+
 app.use(express.json());
 
-let sock;
-
-async function startBot() {
-    const { state, saveCreds } = await useMultiFileAuthState("auth");
-
-    sock = makeWASocket({
-        auth: state,
-        printQRInTerminal: true
-    });
-
-    sock.ev.on("creds.update", saveCreds);
-
-    sock.ev.on("connection.update", (update) => {
-        const { connection, lastDisconnect } = update;
-
-        if (update.qr) {
-            console.log("🔳 QR CODE:");
-            console.log(update.qr);
-        }
-
-        if (connection === "open") {
-            console.log("✅ WHATSAPP CONNECTED");
-        }
-
-        if (connection === "close") {
-            const code = lastDisconnect?.error?.output?.statusCode;
-
-            const shouldReconnect = code !== DisconnectReason.loggedOut;
-
-            console.log("❌ Connection closed. reconnect:", shouldReconnect);
-
-            if (shouldReconnect) startBot();
-        }
-    });
-}
-
-startBot();
-
-// SEND MESSAGE API
-app.post("/send", async (req, res) => {
-    try {
-        const { number, message } = req.body;
-
-        if (!sock) {
-            return res.status(500).json({ error: "Bot belum siap" });
-        }
-
-        const jid = number + "@s.whatsapp.net";
-
-        await sock.sendMessage(jid, { text: message });
-
-        res.json({ status: "sent" });
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: err.message });
-    }
-});
-
-// TEST ROUTE
 app.get("/", (req, res) => {
-    res.send("WA Bot aktif");
+    res.send("BOT HIDUP ✅");
 });
 
-// PORT RAILWAY
+app.post("/send", (req, res) => {
+    res.json({
+        status: "ok",
+        number: req.body.number,
+        message: req.body.message
+    });
+});
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
