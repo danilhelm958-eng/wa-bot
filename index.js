ab7dc5da72523aa261df49713813d55a12152480
danilhelm958-eng <danilhelm958@gmail.com>
2026-06-07 13:42:30 +0700

    index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.post("/send", (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log("Server jalan di port " + PORT);
