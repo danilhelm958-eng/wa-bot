f05f1d421b029e68353300fcb3665ef4355f3cfe
danilhelm958-eng <danilhelm958@gmail.com>
2026-06-07 15:48:54 +0700

    index.js

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ app.get("/", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-    res.json({
+    console.log("HEADERS:", req.headers);
+    console.log("BODY:", req.body);
+
+    res.status(200).json({
         status: "ok",
-        number: req.body.number,
-        message: req.body.message
+        body: req.body
     });
 });
 
